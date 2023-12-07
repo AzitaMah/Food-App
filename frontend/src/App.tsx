87f6317a993819d09f5d4cabb7befe14cd5c8839
Anna Rinck <ar160@hdm-stuttgart.de>
2023-12-07 11:23:24 +0100

    frontend: replace old project with new version containing routing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,75 +1,81 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Typography from "@mui/material/Typography";
-import {AccountCircle} from "@mui/icons-material";
-import Box from "@mui/material/Box";
-import {Drawer, List, ListItem, ListItemText} from "@mui/material";
-import ButtonBaseDemo from "./components/Homepage/Homepage";
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import {
+    BottomNavigation,
+    BottomNavigationAction,
+    Box,
+    CssBaseline,
+    Paper,
+} from "@mui/material";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import SwipeIcon from '@mui/icons-material/Swipe';
 
-function App() {
+//Fixed Tabbar which is always displayed
+function FixedBottomNavigation() {
+    const [value, setValue] = React.useState(0);
+    const ref = React.useRef<HTMLDivElement>(null);
+    React.useEffect(() => {
+        (ref.current as HTMLDivElement).ownerDocument.body.scrollTop = 0;
+    }, [value,]);
 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-    const toggleDrawer = (open: boolean) => {
-        setIsDrawerOpen(open);
-    };
-
-  return (
-    <div className="App">
-        {/* Toolbar */}
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar
-                    //style={{ backgroundColor: '#282c34' }}
+    return (
+        <Box sx={{pb: 7}} ref={ref}>
+            <CssBaseline/>
+            <Paper sx={{position: 'fixed', bottom: 0, left: 0, right: 0}} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
                 >
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        onClick={() => toggleDrawer(true)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Dating-Food App
-                    </Typography>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        color="inherit"
-                    >
-                        <AccountCircle />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
+                    <BottomNavigationAction label="Swipe" href="/foodswipe"
+                                            icon={<SwipeIcon/>}></BottomNavigationAction>
+                    <BottomNavigationAction label="Matches" href="/matches"
+                                            icon={<FavoriteBorderIcon/>}></BottomNavigationAction>
+                </BottomNavigation>
+            </Paper>
         </Box>
-        <header className="App-header">
+    );
+}
 
-            {/* Food Swipe Button */}
-            <div className="display: 'flex', justifyContent: 'center', alignItems: 'center' height: '100vh'">
-                <ButtonBaseDemo></ButtonBaseDemo>
-            </div>
-        </header>
-        <Drawer open={isDrawerOpen} onClose={() => toggleDrawer(false)} >
-            <List>
-                <ListItem>
-                    <ListItemText primary="Matches" />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary="Logout" />
-                </ListItem>
-            </List>
-        </Drawer>
-    </div>
-  );
+//different toolbars for different screens
+const Toolbar = () => <p>Toolbar</p>
+const FoodSwipeToolbar = () => <p>FoodSwipeToolbar</p>
+
+//placeholders for main components
+const HomePage = () => <p>Homepage</p>
+const LoginPage = () => <p>Login</p>
+const Matches = () => <p>Matches</p>
+const Profile = () => <p>Profile</p>
+const FoodSwipe = () => <p>FoodSwipe</p>
+
+const NotFoundPage = () => <p>404</p>
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <aside>
+                <Routes>
+                    <Route path="/" element={<Toolbar/>}/>
+                    <Route path="/foodswipe" element={<FoodSwipeToolbar/>}/>
+                </Routes>
+            </aside>
+            <main>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/matches" element={<Matches/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+
+                    <Route path="/foodswipe" element={<FoodSwipe/>}/>
+                    <Route path="*" element={<NotFoundPage/>}/>
+                </Routes>
+                <FixedBottomNavigation></FixedBottomNavigation>
+            </main>
+        </Router>
+    );
 }
 
 export default App;
