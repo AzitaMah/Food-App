56d06e1814e8c489b06057f18a8b16cd96885e9b
Ai Nhu Dinh <ad101@hdm-stuttgart.de>
2023-12-07 15:31:12 +0100

    backend: add CORS configuration backend

--- backend/src/main/java/com/datingfood/backend/BackendApplication.java
+++ backend/src/main/java/com/datingfood/backend/BackendApplication.java
@@ -3,12 +3,25 @@ package com.datingfood.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class) // stops default Spring security login
 public class BackendApplication {
 
-	public static void main(final String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST");
+            }
+        };
+    }
 
 }
