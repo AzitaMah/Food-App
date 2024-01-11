0824f71497eb256133e7b36e4fb02de41e9f04ad
Anna Rinck <ar160@hdm-stuttgart.de>
2024-01-11 21:10:16 +0100

    frontend: adjust topbar, add navigation

--- frontend/src/components/Toolbars/LoginToolbar/LoginToolbar.tsx
+++ frontend/src/components/Toolbars/LoginToolbar/LoginToolbar.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import styles from './LoginToolbar.module.css';
-import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import {AppBar, Toolbar, Typography} from "@mui/material";
 
 interface LoginToolbarProps {}
 
@@ -9,11 +8,8 @@ const LoginToolbar: FC<LoginToolbarProps> = () => (
   <div className={styles.LoginToolbar} data-testid="LoginToolbar">
       <AppBar position="static" sx={{ backgroundColor: 'lightgrey' }}>
           <Toolbar variant="dense">
-              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                  <MenuIcon />
-              </IconButton>
               <Typography variant="h6" color="inherit" component="div">
-                  Login Dating-Food-App
+                  Login
               </Typography>
           </Toolbar>
       </AppBar>
--- frontend/src/components/Toolbars/ProfileToolbar/ProfileToolbar.tsx
+++ frontend/src/components/Toolbars/ProfileToolbar/ProfileToolbar.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import styles from './ProfileToolbar.module.css';
-import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import {AppBar, Toolbar, Typography} from "@mui/material";
 
 interface ProfileToolbarProps {}
 
@@ -9,9 +8,6 @@ const ProfileToolbar: FC<ProfileToolbarProps> = () => (
   <div className={styles.ProfileToolbar} data-testid="ProfileToolbar">
       <AppBar position="static" sx={{ backgroundColor: 'lightgrey' }}>
           <Toolbar variant="dense">
-              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                  <MenuIcon />
-              </IconButton>
               <Typography variant="h6" color="inherit" component="div">
                   Your Profile
               </Typography>
