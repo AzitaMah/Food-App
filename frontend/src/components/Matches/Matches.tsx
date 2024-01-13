c658b3da8a4e311597ee6c7976117a6b82cc8c08
Anna Rinck <ar160@hdm-stuttgart.de>
2024-01-13 17:45:31 +0100

    frontend: implement matches component.

--- frontend/src/components/Matches/Matches.tsx
+++ frontend/src/components/Matches/Matches.tsx
@@ -1,12 +1,62 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import styles from './Matches.module.css';
+import {Avatar, Divider, FormControlLabel, List, ListItem, ListItemAvatar, ListItemText, Switch} from "@mui/material";
+import ImageIcon from "@mui/icons-material/Image";
 
-interface MatchesProps {}
+interface MatchesProps {
+}
 
-const Matches: FC<MatchesProps> = () => (
-  <div className={styles.Matches} data-testid="Matches">
-    Matches Component
-  </div>
-);
+interface Person {
+    userName: string;
+    firstName: string;
+    contactInformation: string;
+    birthDate: string;
+    image: string;
+}
+
+const dummyPerson: Person = {
+    userName: "maxi_food",
+    firstName: "Max",
+    contactInformation: "insta: maxixmuster",
+    birthDate: "01.08.2007",
+    image: "3",
+}
+
+const dummyPersons: Person[] = Array.from({length: 15}, () => dummyPerson);
+
+const Matches: FC<MatchesProps> = () => {
+    const [onlyMatches, setOnlyMatches] = useState(false);
+
+    const handleChange = () => {
+        setOnlyMatches((prev) => !prev);
+    };
+
+    const listItems = dummyPersons.map(person => (
+        <div key={person.userName}>
+            <ListItem>
+                <ListItemAvatar>
+                    <Avatar>
+                        <ImageIcon/>
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={onlyMatches ? person.firstName : person.userName}
+                              secondary={onlyMatches ? person.contactInformation : person.birthDate}/>
+            </ListItem>
+            <Divider/>
+        </div>
+    ));
+
+    return (
+        <div className={styles.Matches} data-testid="Matches">
+            <FormControlLabel
+                control={<Switch checked={onlyMatches} onChange={handleChange}/>}
+                label="Only show matches"
+            />
+            <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+                {listItems}
+            </List>
+        </div>
+    );
+};
 
 export default Matches;
