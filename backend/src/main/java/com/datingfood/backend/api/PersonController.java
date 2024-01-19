667d7ef31f62639875f74d9bd3453ff0bbc62215
Ai Nhu Dinh <ad101@hdm-stuttgart.de>
2024-01-19 16:36:25 +0100

    backend: add admin DELETE endpoint #13

--- backend/src/main/java/com/datingfood/backend/api/PersonController.java
+++ backend/src/main/java/com/datingfood/backend/api/PersonController.java
@@ -5,12 +5,7 @@ import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.datingfood.backend.dto.FoodRequestDTO;
 import com.datingfood.backend.entities.Person;
@@ -61,4 +56,13 @@ public class PersonController {
         return ResponseEntity.ok(personList);
     }
 
+    @DeleteMapping("admin/person/{username}")
+    ResponseEntity<String> deletePerson(@PathVariable String username) {
+        try {
+            personService.deletePersonEntry(username);
+            return ResponseEntity.ok().build();
+        }catch (NoSuchElementException exception){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- backend/src/main/java/com/datingfood/backend/entities/Person.java
+++ backend/src/main/java/com/datingfood/backend/entities/Person.java
@@ -32,7 +32,7 @@ public class Person {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     @JsonIgnore
