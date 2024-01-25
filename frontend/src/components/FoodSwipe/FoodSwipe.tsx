c1571fca916eb8e4576e33c9d655eb7c492d0b58
Anna Rinck <ar160@hdm-stuttgart.de>
2024-01-25 09:45:53 +0100

    frontend: food change functionality

--- frontend/src/components/FoodSwipe/FoodSwipe.tsx
+++ frontend/src/components/FoodSwipe/FoodSwipe.tsx
@@ -1,49 +1,55 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './FoodSwipe.module.css';
-import {Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Stack, Typography} from "@mui/material";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Stack, Typography } from "@mui/material";
 import NotInterestedIcon from "@mui/icons-material/NotInterested";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 interface FoodSwipeProps {}
 
 const foodOptions: string[] = ["Sushi", "Burger", "Pizza", "Pasta", "Fast Food"];
-var currentIndex: number = 0;
-function handleDislike() {
-    currentIndex += 1;
-    console.log(foodOptions[currentIndex])
 
-}
+const FoodSwipe: FC<FoodSwipeProps> = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-const FoodSwipe: FC<FoodSwipeProps> = () => (
-    <Stack direction="row" justifyContent="center" sx={{ p: "20px", }}>
-    <div className={styles.FoodSwipe} data-testid="FoodSwipe">
-            <Card sx={{maxWidth: 345}}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        src="frontend/src/assets/sushi.png"
-                        alt="sushi"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            { foodOptions[currentIndex] }
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Stack direction="row" justifyContent="space-between" flex="content">
-                    <Button size="small" color="primary" onClick={handleDislike}>
-                        <NotInterestedIcon/>
-                    </Button>
-                    <Button size="small" color="primary" href="/swipe-list">
-                        <FavoriteIcon/>
-                    </Button>
-                    </Stack>
-                </CardActions>
-            </Card>
-    </div>
-    </Stack>
-);
+    const handleDislike = () => {
+        setCurrentIndex(currentIndex + 1);
+        if (currentIndex + 1 >= foodOptions.length) {
+            setCurrentIndex(0);
+        }
+        console.log(foodOptions[currentIndex]);
+    };
+
+    return (
+        <Stack direction="row" justifyContent="center" sx={{ p: "20px" }}>
+            <div className={styles.FoodSwipe} data-testid="FoodSwipe">
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            src="frontend/src/assets/sushi.png"
+                            alt="sushi"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {foodOptions[currentIndex]}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Stack direction="row" justifyContent="space-between" flex="content">
+                            <Button size="small" color="primary" onClick={handleDislike}>
+                                <NotInterestedIcon />
+                            </Button>
+                            <Button size="small" color="primary" href="/swipe-list">
+                                <FavoriteIcon />
+                            </Button>
+                        </Stack>
+                    </CardActions>
+                </Card>
+            </div>
+        </Stack>
+    );
+};
 
 export default FoodSwipe;
